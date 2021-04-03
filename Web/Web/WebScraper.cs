fa1c12bb821b73ea7d2a96d039abb10a02a1a1cd
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-04-03 00:02:39 -0400

    Fixed STMP client
    will actually now send a text

--- Web/Web/WebScraper.cs
+++ Web/Web/WebScraper.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 using System.Net.Mail;
+using System.Net;
 
 namespace Web
 {
@@ -16,23 +17,35 @@ namespace Web
             Thread.Sleep(5000);
             //*[@id="accordion-d12e112"]
 
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("saumenu@gmail.com", "Andr1234!"),
+                EnableSsl = true,
+            };
+
+            //smtpClient.Send("email", "recipient", "subject", "body");
+
+
+
             var message = new MailMessage();
             message.From = new MailAddress("saumenu@gmail.com");
 
-            message.To.Add(new MailAddress("9095226733@vtext.com"));//See carrier destinations below
+            message.To.Add(new MailAddress("7577554736@vtext.com"));//See carrier destinations below
                                                                       //message.To.Add(new MailAddress("5551234568@txt.att.net"));
 
             //message.CC.Add(new MailAddress("carboncopy@foo.bar.com"));
             message.Subject = "Test";
-            message.Body = "This is a test";
+            message.Body = "User Tristan Deschamps has been disconnected from Southern's Meal Plan Servers. Please contact Wright Hall before you attempt to purchase any more meal plan items.";
 
-            var client = new SmtpClient();
-            client.Send(message);
+           // var client = new SmtpClient();
+            smtpClient.Send(message);
 
 
 
             /*
-                ATT: Compose a new email and use the recipient's 10-digit wireless phone number, followed by @txt.att.net. For example, 5551234567@txt.att.net.
+                ATT: Compose a new email and use@txt.att.net. For example, 5551234567@txt.att.net.
                 Verizon: Similarly, ##@vtext.com
                 Sprint: ##@messaging.sprintpcs.com
                 TMobile: ##@tmomail.net
