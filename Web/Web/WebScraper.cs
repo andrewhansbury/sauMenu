534b239943078d77b06f1a058dc963ca68dc5968
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-04-03 01:37:44 -0400

    deleted useless comments

--- Web/Web/WebScraper.cs
+++ Web/Web/WebScraper.cs
@@ -15,9 +15,7 @@ namespace Web
             driver.Navigate().GoToUrl("https://www.southern.edu/administration/food/index.html");
             driver.FindElement(By.XPath("//*[@id=\"SouthernFramework\"]/div/div/main/div[2]/div/div/div[2 ]/a")).Click();
             Thread.Sleep(5000);
-            //*[@id="accordion-d12e112"]
-
-
+       
             var smtpClient = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
@@ -25,19 +23,12 @@ namespace Web
                 EnableSsl = true,
             };
 
-            //smtpClient.Send("email", "recipient", "subject", "body");
-
-
-
+   
             var message = new MailMessage();
             message.From = new MailAddress("saumenu@gmail.com");
 
-            message.To.Add(new MailAddress("7577554736@vtext.com"));//See carrier destinations below
-                                                                      //message.To.Add(new MailAddress("5551234568@txt.att.net"));
-
-            //message.CC.Add(new MailAddress("carboncopy@foo.bar.com"));
-            message.Subject = "Test";
-            message.Body = "User Tristan Deschamps has been disconnected from Southern's Meal Plan Servers. Please contact Wright Hall before you attempt to purchase any more meal plan items.";
+            message.To.Add(new MailAddress("9095226733@vtext.com"));
+            message.Body = "Sarah Jane is a fat noob ";
 
            // var client = new SmtpClient();
             smtpClient.Send(message);
