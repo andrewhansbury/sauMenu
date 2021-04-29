3eec20609a78d42b92f77be65d8fcedc32d5dccb
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2021-04-29 13:08:47 -0400

    beginning database connection

---
+++ Web/Web/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Web
+{
+    class DatabaseConnection
+    {
+        
+        public void databaseConnect()
+        {
+            string connectionString = @"Data Source=DESKTOP-3M3LSKK\SQLEXPRESS;Initial Catalog=saumenudb;Integrated " +
+            "Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+        
+
+        
+
+       
+
+    }
+}
+ 
--- Web/Web/Program.cs
+++ Web/Web/Program.cs
@@ -1,14 +1,28 @@
 ﻿
 
+using System.Data.SqlClient;
+using System;
+
 namespace Web
 {
     class Program
     {
+      
+
         static void Main(string[] args)
         {
+            
+
             WebScraper meals = new WebScraper();
 
-            meals.GetMeals();
+            //meals.GetMeals();
+
+            DatabaseConnection database = new DatabaseConnection();
+            database.databaseConnect();
+            
+           
+
+            
             //SmsSender sms = new SmsSender();
             //sms.SendSMS(meals.GetMeals());
         }
